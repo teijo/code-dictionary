2b55b599467f5450b556108afcaed3c9492207be
Teijo Laine <teijo@aropupu.fi>
2015-10-10 19:15:29 +0300

    Run ESLint in watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,19 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var watch = require('gulp-watch');
+var eslint = require('gulp-eslint');
 
-gulp.task('bundle', function() {
+gulp.task('lint', function() {
+  return gulp.src(['src/*.js'])
+      .pipe(eslint())
+      .pipe(eslint.format());
+});
+
+gulp.task('bundle', ["lint"], function() {
   return browserify({
-        extensions: ['.js', '.jsx'],
-        entries: 'src/index.js'
-      })
+    extensions: ['.js', '.jsx'],
+    entries: 'src/index.js'
+  })
       .transform(babelify.configure({
         ignore: /(bower_components)|(node_modules)/
       }))
